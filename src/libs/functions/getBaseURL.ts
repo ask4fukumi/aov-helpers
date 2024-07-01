39d052ea3e6bdedda0abbd1db839bc3c091a7973
Fukumi <75573917+ask4fukumi@users.noreply.github.com>
2024-07-02 01:17:13 +0700

    feat: separate `preview` branch versioning and `master` branch versioning

--- src/libs/functions/getBaseURL.ts
+++ src/libs/functions/getBaseURL.ts
@@ -1,7 +1,15 @@
 const getBaseURL = function () {
   if (typeof window !== "undefined") return ""
-  else if (process.env.NEXT_PUBLIC_VERCEL_URL)
+  else if (
+    process.env.NEXT_PUBLIC_VERCEL_URL
+    && process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+  )
     return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  else if (
+    process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+    && process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+  )
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
   else return `http://localhost:${process.env.PORT}`
 }
 
