c59ec785a220590b86dbeeed7269482b461a3903
Fukumi <75573917+ask4fukumi@users.noreply.github.com>
2024-09-21 15:57:52 +0700

    feat: code style config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import("prettier").Config} */
 const prettierConfig = {
-    plugins: [await import("prettier-plugin-tailwindcss")],
-    tailwindAttributes: [
-        "className",
-        "class",
-        "ngClass",
-        ".*Styles.*",
-        ".*Classes.*",
-    ],
-    tailwindFunctions: ["cn"],
-    semi: false,
-    trailingComma: "all",
-    singleQuote: false,
-    printWidth: 80,
-    useTabs: false,
-    tabWidth: 2,
+  plugins: [await import("prettier-plugin-tailwindcss")],
+  tailwindAttributes: [
+    "className",
+    "class",
+    "ngClass",
+    "class:list",
+    ".*(Class|Style|Variant).*",
+  ],
+  tailwindFunctions: ["cn", "cva", "tv"],
+  semi: false,
+  trailingComma: "all",
+  singleQuote: false,
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
 }
 
 export default prettierConfig
